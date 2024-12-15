4d87a11430fa04b31e2a5afcdd975b4fa84956d1
Ellvius <lishinvs6541@gmail.com>
2024-12-15 23:32:32 +0530

    Dynamically generate host url and redirect site

--- backend/src/controllers/urlController.js
+++ backend/src/controllers/urlController.js
@@ -1,18 +1,18 @@
-import { insertUrl, getShortUrl, increaseHitCount } from '../models/urlModel.js';
+import { insertUrl, getShortUrl, increaseHitCount, getExistingLongUrl } from '../models/urlModel.js';
 import { generateShortUrl } from '../utils/generateShortUrl.js';
 
 export const shortenUrl = async (req, res) => {
   const { longUrl } = req.body;
 
   try {
-    const existingUrl = await pool.query('SELECT * FROM urls WHERE long_url = $1', [longUrl]);
+    const existingUrl = await getExistingLongUrl(longUrl);
     if (existingUrl.rows.length > 0) {
-      return res.status(200).json({ shortUrl: `http://localhost:5000/${existingUrl.rows[0].short_url}` });
+      return res.status(200).json({ shortUrl: `${process.env.HOST_URL}/redirect/${existingUrl.rows[0].short_url}` });
     }
 
     const shortUrl = generateShortUrl();
     const newUrl = await insertUrl(longUrl, shortUrl);
-    res.status(201).json({ shortUrl: `http://localhost:5000/${newUrl.short_url}` });
+    res.status(201).json({ shortUrl: `${process.env.HOST_URL}/redirect/${newUrl.short_url}` });
   } catch (err) {
     console.error('Error creating short URL:', err.message);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -30,7 +30,7 @@ export const redirectToLongUrl = async (req, res) => {
 
     const hitCount = await increaseHitCount(shortUrl);
     if (hitCount % 10 === 0) {
-      return res.redirect('https://google.com');
+      return res.redirect(process.env.REDIRECT_SITE);
     }
 
     res.redirect(urlData.long_url);

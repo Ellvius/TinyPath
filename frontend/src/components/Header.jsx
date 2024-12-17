6d7a9272712551572269fb5fe5c02179505cfd3e
Ellvius <lishinvs6541@gmail.com>
2024-12-17 23:48:52 +0530

    update name

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <header className="bg-secondary">
-            <h1 className="text-primary font-bold text-7xl p-12 pb-0 text-center">CLINK</h1>
+            <h1 className="text-primary font-bold text-7xl p-12 pb-0 text-center">TINY PATH</h1>
             <p className="p-8 text-center text-primary font-semibold text-xl">Welcome to your URL shortener app!</p>
         </header>
     )
